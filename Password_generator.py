7c8e3ebfdf6b85cda399344c2580f53751e364b8
Adarsh Shirawalmath <114558126+adarshxs@users.noreply.github.com>
2022-10-04 23:03:05 +0530

    Revert "Password_generator"

--- Password_generator.py
+++
@@ -1,8 +0,0 @@
-import random
-import string
-
-def get_random_string(length):
-    result_str = ''.join(random.choice(string.ascii_letters) for i in range(length))
-    print(result_str)
-
-get_random_string(12)
